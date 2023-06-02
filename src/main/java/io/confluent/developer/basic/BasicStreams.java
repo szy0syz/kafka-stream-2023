35ed841b32e04c1f9bae43f4a74c5e9efd72710b
Zhenyu Shi <zhenyu.shi@compass.com>
2023-06-02 23:41:27 +0800

    Update logic of BasicStreams

--- src/main/java/io/confluent/developer/basic/BasicStreams.java
+++ src/main/java/io/confluent/developer/basic/BasicStreams.java
@@ -31,14 +31,17 @@ public class BasicStreams {
         // Using the StreamsBuilder from above, create a KStream with an input-topic
         // and a Consumed instance with the correct
         // Serdes for the key and value HINT: builder.stream and Serdes.String()
-        KStream<String, String> firstStream = null;
+        KStream<String, String> firstStream = builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()));
 
         firstStream.peek((key, value) -> System.out.println("Incoming record - key " + key + " value " + value))
                 // filter records by making sure they contain the orderNumberStart variable from above HINT: use filter
+                .filter((key, value) -> value.contains(orderNumberStart))
                 // map the value to a new string by removing the orderNumberStart portion HINT: use mapValues
+                .mapValues(value -> value.substring(value.indexOf("-") + 1))
                 // only forward records where the value is 1000 or greater HINT: use filter and Long.parseLong
-                .peek((key, value) -> System.out.println("Outgoing record - key " + key + " value " + value));
+                .peek((key, value) -> System.out.println("Outgoing record - key " + key + " value " + value))
                 // Write the results to an output topic defined above as outputTopic HINT: use "to" and Produced and Serdes.String()
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
 
         try (KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps)) {
             final CountDownLatch shutdownLatch = new CountDownLatch(1);
